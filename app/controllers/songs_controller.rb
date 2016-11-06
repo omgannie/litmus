e1821f63f6704a5f4f000792fe50a921de40ff3b
Keri Gannon <email": null>
2016-11-06 16:31:02 -0600

    Add div wrappers around homepage, remove excess methods from controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,7 @@
 class SongsController < ApplicationController
-  def index
-  end
-
   def search_song
     genre_id = params[:genre].to_i
     genre = Genre.find_by(id: genre_id)
     redirect_to "/passages"
-  end
-
-  private
-  def song_params
   end
 end
