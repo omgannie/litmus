2893c3747165218c95971ecf08b8d5ba296fe91a
sayamkim <sayamkim@gmail.com>
2016-11-06 13:38:26 -0600

    Add api logic.

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -2,4 +2,13 @@ class Passage < ActiveRecord::Base
   validates :body, presence: true
 
   has_one :emotion, as: :emotionable
+
+  def analyze_passage
+    if !params[:passage].empty?
+      init = ToneAnalyzer.new(ENV["WATSON_USERNAME"], ENV["WATSON_PASSWORD"])
+      @response = init.analyze(params[:passage])
+    else
+      redirect_to root_path
+    end
+  end
 end
