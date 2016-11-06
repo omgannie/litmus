b7dc144fab1888f26c1b679a667f58f4759a31f1
Keri Gannon <email": null>
2016-11-06 16:25:28 -0600

    Add categories to genre on seed file, add categories array column to genre

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,14 @@
 class SongsController < ApplicationController
+  def index
+  end
+
+  def search_song
+    genre_id = params[:genre].to_i
+    genre = Genre.find_by(id: genre_id)
+    redirect_to "/passages"
+  end
+
+  private
+  def song_params
+  end
 end
