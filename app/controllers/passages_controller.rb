c3f04ab792299c73a1ad2087eb4bac86a2e9d743
sayamkim <sayamkim@gmail.com>
2016-11-07 05:06:30 -0600

    Add passages logic for creaete. Still have to figure out emotion field values.

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -1,13 +1,16 @@
 class PassagesController < ApplicationController
   def index
     @genre_options =  Genre.all.map{ |genre| [genre.name, genre.id] }
+    @passage = Passage.new
   end
 
   def create
     @passage = Passage.new(passage_params)
 
     if @passage.save
-      # TODO: Run through Watson analyzer
+      tone_analysis = passage.analyze_passage
+      tone_results = Passage.emotion_tone(tone_analysis)
+      primary_tone = Passage.primary_emotion(tone_results)
     end
     redirect_to "/passages"
   end
@@ -15,7 +18,7 @@ class PassagesController < ApplicationController
   def show
     passage_object = Passage.find(params[:id])
     watson_object = passage_object.analyze_passage
-    @tone = Passage.emotion_tone(watson_object)
+    tone = Passage.emotion_tone(watson_object)
   end
 
   private
