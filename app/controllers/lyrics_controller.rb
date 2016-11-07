63024f5d55ed15e4c2a0ee3b1afd5a031066ac54
Keri Gannon <email": null>
2016-11-07 14:41:12 -0600

    Revert back to search_lyrics method in controller

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -3,14 +3,13 @@ class LyricsController < ApplicationController
   def search_lyrics
     songs = Song.recent
     lyric = Lyric.new
-    lyrics = []
     all_track_ids = []
     songs.each do |song|
       result = lyric.get_track_id(song.artist_name, song.song_title)
       all_track_ids.push(result[0])
     end
-  all_track_ids
-  [99109152, 13912023, 56340444, 44273080, 100529425]
+
+    lyrics = []
     redirect_to '/passages'
   end
 end
