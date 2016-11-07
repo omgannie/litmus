b8d25e4925e4759a9feb7398794c53c5531d571e
omgannie <annie_oh@icloud.com>
2016-11-06 19:03:20 -0600

    Latest commit for push

--- app/assets/javascripts/watson_color.js
+++ app/assets/javascripts/watson_color.js
@@ -24,6 +24,7 @@ var color = {
   hue: "",
   saturation: "",
   lightness: "",
+  length: "",
 };
 
 // formats hsb for css input
--- app/assets/javascripts/watson_gradient.js
+++ app/assets/javascripts/watson_gradient.js
@@ -1,13 +1,15 @@
 // Set up SVG container
 var svg = d3.select('body')
             .append('svg')
+            .attr('width', 500)
+            .attr('height', 100);
 
 //Append a defs (for definition) element to SVG
 var defs = svg.append("defs");
 
 //Append a linearGradient element to the defs and give it a unique id
 var linearGradient = defs.append("linearGradient")
-.attr("id", "linear-gradient");
+                         .attr("id", "linear-gradient");
 
 //Horizontal gradient
 linearGradient
@@ -16,12 +18,23 @@ linearGradient
 .attr("x2", "100%")
 .attr("y2", "0%");
 
-//Set the color for the start (0%)
+//Set the color for the start (anger section) (0%)
 linearGradient.append("stop")
-.attr("offset", "0%")
-.attr("stop-color", "#ffa474"); //light blue
+              .attr("offset", "0%")
+              .attr("stop-color", "#ffa474"); //red
 
-//Set the color for the end (100%)
+//Set the color for the middle left (disgust section) (30%)
+linearGradient.append("stop")
+              .attr("offset", "0%")
+              .attr("stop-color", "#ffa474"); //green
+
+//Set color for mid - middle (fear section) (50%)
+linearGradient.append('stop')
+              .attr('offset', '0%')
+
+//Set color for middle right (joy) (70%)
+
+//Set the color for the end (sadness) (100%)
 linearGradient.append("stop")
 .attr("offset", "100%")
 .attr("stop-color", "#8b0000"); //dark blue
