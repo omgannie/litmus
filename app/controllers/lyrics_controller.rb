a102fc00c94f700d051450591d320b8bacb04f8b
Keri Gannon <email": null>
2016-11-07 14:35:03 -0600

    Add method in lyrics model to return 5 track ids

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,11 +1,16 @@
 class LyricsController < ApplicationController
-  def search_lyrics
-    @songs = Song.recent
-    @lyrics = Lyric.new
-    @songs.each do |song|
-      p song
-    end
 
+  def search_lyrics
+    songs = Song.recent
+    lyric = Lyric.new
+    lyrics = []
+    all_track_ids = []
+    songs.each do |song|
+      result = lyric.get_track_id(song.artist_name, song.song_title)
+      all_track_ids.push(result[0])
+    end
+  all_track_ids
+  [99109152, 13912023, 56340444, 44273080, 100529425]
     redirect_to '/passages'
   end
 end
--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -4,8 +4,15 @@ class Lyric < ActiveRecord::Base
   belongs_to :song
   has_one :emotion, as: :emotionable
 
-  def get_lyrics
+  def get_track_id(artist_name, song_title)
   	MusixMatch::API::Base.api_key = ENV['MUSIX_MATCH']
-  	
+    response = MusixMatch.search_track(q_track: song_title, q_artist: artist_name)
+    if response.status_code == 200
+      track_ids = []
+      response.each do |track|
+        track_ids.push(track.track_id)
+      end
+    end
+    track_ids
   end
 end
