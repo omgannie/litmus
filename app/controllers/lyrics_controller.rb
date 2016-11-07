855a77c9cf20845b1d09dc24067e111a766b72ec
Keri Gannon <email": null>
2016-11-07 12:31:06 -0600

    Add 5 most recent songs in Song model

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,2 +1,5 @@
 class LyricsController < ApplicationController
+  def search_lyrics
+    @songs = Song.recent
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 require 'rspotify'
 
 class Song < ActiveRecord::Base
+  scope :recent, -> { order("created_at DESC").limit(5) }
   validates :artist_name, :song_title, presence: true
 
   has_one :lyric
@@ -13,8 +14,8 @@ class Song < ActiveRecord::Base
     RSpotify.raw_response = true
     recommendations = RSpotify::Recommendations.generate(limit: 5, seed_genres: hash[:seed_genres])
     response = JSON.parse(recommendations)
-    
-    # return relevant data as 
+
+    # return relevant data as
     metadata_list = []
 		response["tracks"].each do |track|
 			metadata_list << {"artist" => track["artists"][0]["name"], "track" => track["name"]}
