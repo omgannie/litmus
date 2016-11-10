0377091655c01f62f69dbef66e0de7d1a6edafe4
Keri Gannon <email": null>
2016-11-10 16:13:54 -0600

    Rearrange genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -18,9 +18,9 @@ class GenresController < ApplicationController
     @third = []
 
     until index == gather_genres.length
-      if index < 10
+      if index < 9
         @first << gather_genres[index]
-      elsif index < 19
+      elsif index < 18
         @second << gather_genres[index]
       else
         @third << gather_genres[index]
